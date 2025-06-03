e4c84116eece52de3f308a1c6ee93dff73455d13
Edzio96 <ebrearleysmith@gmail.com>
2025-06-03 16:29:33 +0200

    G: char_count in stats, main modified

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_counts
+
 def main():
 	book_path = "books/frankenstein.txt"
 	text = get_book_text(book_path)
 	num_words = get_num_words(text)
+	char_counts = get_char_counts(text)
 	
 	print(f"{num_words} words found in the document")
+	print(f"Character counts: {char_counts}")
 	
 def get_book_text(filepath):
 	with open(filepath) as f:
 		return f.read()
-
-#def get_num_words(text):
-	#words = text.split()
-	#return len(words)
 
 #if __name__ == "__main__":
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,10 @@
 def get_num_words(text):
 	words = text.split()
 	return len(words)
+
+def get_char_counts(text):
+	counts = {}
+	lower_text = text.lower()
+	for char in lower_text:
+		counts[char] = counts.get(char, 0) + 1
+	return counts
