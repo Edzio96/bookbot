c79c85b5bc5c82fceef7d60c8d9bbc84bed684cd
Edzio96 <ebrearleysmith@gmail.com>
2025-06-07 02:52:45 +0200

    H: new fn to stats, print neat report

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from stats import get_num_words, get_char_counts
+from stats import get_num_words, get_char_counts, get_ordered_char_counts, print_report
 
 def main():
 	book_path = "books/frankenstein.txt"
 	text = get_book_text(book_path)
-	num_words = get_num_words(text)
-	char_counts = get_char_counts(text)
-	
-	print(f"{num_words} words found in the document")
-	print(f"Character counts: {char_counts}")
-	
+	#num_words = get_num_words(text)
+	#char_counts = get_char_counts(text)
+	#ordered_char_counts = get_ordered_char_counts(text)
+	print_report(book_path, text)
+		
 def get_book_text(filepath):
 	with open(filepath) as f:
 		return f.read()
 
 #if __name__ == "__main__":
+
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,30 @@ def get_char_counts(text):
 	for char in lower_text:
 		counts[char] = counts.get(char, 0) + 1
 	return counts
+
+def get_ordered_char_counts(text):
+	char_counts = get_char_counts(text)
+	char_list = []
+	for char in char_counts:
+		if not char.isalpha():
+			continue
+		num = char_counts[char]
+		char_list.append({'char': char, 'num': num})	
+			
+	char_list.sort(key=lambda x: x['num'], reverse=True)
+	return char_list
+	
+
+def print_report(book_path, text):
+	num_words = get_num_words(text)
+	char_list = get_ordered_char_counts(text)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {book_path}")
+	print("----------- Word Count ----------")
+	print(f"Found {num_words} total words")
+	print("--------- Character Count -------")
+	for dict in char_list:
+		print(f"{dict['char']}: {dict['num']}")
+	print("============= END ===============")
+
+	
